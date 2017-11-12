12d80263a265a553ddc13aa514b1ee7a05281004
Jurek Wawro <jurek@tenar.pl>
2017-11-12 11:31:23 +0100

    move bryk to https://github.com/galicea/bryk

--- bryk/bryk/src/build_interface.py
+++
@@ -1,7 +0,0 @@
-with open('interface_pl.html', 'r') as input, open('interface-new.js', 'w') as output:
-    contents = input.read()
-    cleaned_contents = contents.replace('"', '\\"').replace('\n', '')
-    js_contents = '''
-BlockPyInterface = "{interface_code}";
-'''.format(interface_code=cleaned_contents)
-    output.write(js_contents)
